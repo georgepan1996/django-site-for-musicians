a8d6485e152ffe8c71f5d414400c549e193341cb
giwrgospan <giwrgospan277@gmail.com>
2020-11-07 10:47:58 +0200

    Django site update

--- musician_finder/urls.py
+++ musician_finder/urls.py
@@ -26,7 +26,6 @@ from users_core import views as user_view
 urlpatterns = [
 
     path('admin/', admin.site.urls),
-    path('music/', include('music_up_down_load.urls'), name='music_up_down_load'),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('user/', include('users_core.urls')),
     path('accounts/', include('django.contrib.auth.urls'), name='accounts'),
@@ -38,10 +37,17 @@ urlpatterns = [
     path('lyrics_list/upload', user_view.upload_lyrics, name='upload_lyrics'),
     path('search/', user_view.profile_search, name='profile_search'),
     path('profile/<int:prof_id>', user_view.profile_appearance, name='profile_appearance'),
-]
+    path('messages/', include('postman.urls')),
+    path('song_delete/<int:song_id>', user_view.song_delete, name='song_delete'),
+    path('lyric_delete/<int:lyric_id>',  user_view.lyric_delete, name='lyric_delete'),
+    path('song_list/<int:prof_id>', user_view.other_composition_list, name='other_song_list'),
+    path('lyrics_list/<int:prof_id>', user_view.other_lyrics_list, name='other_lyrics_list'),
 
+
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
+# path('music/', include('music_up_down_load.urls'), name='music_up_down_load'),
